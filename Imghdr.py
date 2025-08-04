fb54aa8456bda68fce27cb3323f5cc1e59c9ca4d
nathanfroe <nathan.froe@gmail.com>
2025-08-04 13:53:01 -0400

    Create Imghdr.py

---
+++ Imghdr.py
@@ -0,0 +1,5 @@
+# imghdr.py - compatibility shim for deprecated module in Python 3.13+
+
+def what(file, h=None):
+    # This is a stub. Your bot doesn't handle images, so we return None.
+    return None
